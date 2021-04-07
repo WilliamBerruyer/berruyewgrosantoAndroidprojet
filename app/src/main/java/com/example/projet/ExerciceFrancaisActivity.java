700929c69c177bc22197f5187bd9271ab93d76e8
William Berruyer <william.berruyer@iut2.univ-grenoble-alpes.fr>
2021-04-07 15:52:30 +0200

    francais maj

--- app/src/main/java/com/example/projet/ExerciceFrancaisActivity.java
+++ app/src/main/java/com/example/projet/ExerciceFrancaisActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.projet.db.DatabaseClient;
 import com.example.projet.db.User;
 import com.example.projet.dbQuestion.DatabaseClientQuestion;
 import com.example.projet.dbQuestion.Question;
+import com.example.projet.QuestionsAdapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExerciceFrancaisActivity extends AppCompatActivity {
@@ -18,7 +21,7 @@ public class ExerciceFrancaisActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 0;
     private DatabaseClientQuestion mdb;
     private QuestionsAdapter adapter;
-    List<Question> questions;
+    private ListView listQuestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +30,57 @@ public class ExerciceFrancaisActivity extends AppCompatActivity {
 
         // Récupération du DatabaseClient
         mdb = DatabaseClientQuestion.getInstance(getApplicationContext());
+
+        // Récupérer les vues
+        listQuestions = findViewById(R.id.listUser);
+
+        // Lier l'adapter au listView
+        adapter = new QuestionsAdapter(this, new ArrayList<Question>());
+        listQuestions.setAdapter(adapter);
+
         // Mise à jour des utilisateurs
-        questions=getQuestions();
-        TextView textQuestion = findViewById(R.id.questionTxt);
-        textQuestion.setText(questions.get(1).getQuestion());
+        getQuestions();
     }
 
+    private void getQuestions() {
+        ///////////////////////
+        // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
+        class GetQuestions extends AsyncTask<Void, Void, List<Question>> {
 
-    protected List<Question> getQuestions(Void... voids) {
-        List<Question> QuestionList = mdb.getAppDatabase().questionDAO().getAll();
-        return QuestionList;
+            @Override
+            protected List<Question> doInBackground(Void... voids) {
+                List<Question> QuestionList = mdb.getAppDatabase()
+                        .questionDAO()
+                        .getAll();
+                return QuestionList;
+            }
+
+            @Override
+            protected void onPostExecute(List<Question> questions) {
+                super.onPostExecute(questions);
+
+                // Mettre à jour l'adapter avec la liste de taches
+                adapter.clear();
+                adapter.addAll(questions);
+
+                // Now, notify the adapter of the change in source
+                adapter.notifyDataSetChanged();
+            }
+        }
+
+        //////////////////////////
+        // IMPORTANT bien penser à executer la demande asynchrone
+        // Création d'un objet de type GetQuestions et execution de la demande asynchrone
+        GetQuestions gt = new GetQuestions();
+        gt.execute();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
 
+        // Mise à jour des questions
+        getQuestions();
 
     }
     @Override
